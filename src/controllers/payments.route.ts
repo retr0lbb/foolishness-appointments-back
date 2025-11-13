b7a3d35b035be8e3598c1bb62005ea844d525c91
retr0lbb <retr0lbb@gmail.com>
2025-11-13 03:58:44 -0300

    feat: memz

--- src/controllers/payments.route.ts
+++ src/controllers/payments.route.ts
@@ -7,8 +7,11 @@ const router = express.Router();
 
 const paymentSchema = z.object({
 	value: z.string(),
-	payment_method: z.literal("card").or(z.literal("cash")),
-	appointment_id: z.string(),
+	payment_method: z
+		.literal("card")
+		.or(z.literal("cash"))
+		.or(z.literal("insurance")),
+	appointment_id: z.number(),
 });
 
 interface Payment extends RowDataPacket, z.infer<typeof paymentSchema> {}
@@ -25,6 +28,22 @@ router.post("/", async (req, res) => {
 			"INSERT INTO payments(value, payment_method, appointment_id) VALUES (?, ?, ?)",
 			[value, payment_method, appointment_id],
 		);
+
+		return res.send(201).send("Payment Method created");
+	} catch (error) {
+		throw error;
+	}
+});
+
+router.get("/", async (req, res) => {
+	try {
+		const conn = await getConnection();
+
+		const [payments] = await conn.query<Payment[]>(
+			"SELECT BIN_TO_UUID(id) as id, value, payment_method, appointment_id from payments;",
+		);
+
+		res.status(200).json({ payments });
 	} catch (error) {
 		throw error;
 	}
