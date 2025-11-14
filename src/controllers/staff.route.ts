c325ae1e750b52cbd0fbce9b872dc36ae15dbf3f
retr0lbb <retr0lbb@gmail.com>
2025-11-14 02:36:08 -0300

    feat: add update route

--- src/controllers/staff.route.ts
+++ src/controllers/staff.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import z from "zod/v4";
+import z, { codec } from "zod/v4";
 import { getConnection } from "../db/client";
 import type { RowDataPacket } from "mysql2";
 
@@ -38,6 +38,31 @@ router.get("/", async (_, res) => {
 		);
 
 		return res.status(200).json({ staffs });
+	} catch (error) {
+		throw error;
+	}
+});
+
+router.patch("/:code", async (req, res) => {
+	const { jobFunction, name } = staffSchema.parse(req.body);
+	try {
+		const conn = await getConnection();
+
+		const [foundStaff] = await conn.query<Staff[]>(
+			"SELECT code, name, job_function FROM staff WHERE code = ?",
+			[req.params.code],
+		);
+
+		if (foundStaff.length !== 1) {
+			return res
+				.status(500)
+				.json({ message: "Duplicated KEYS found in DATABASE" });
+		}
+
+		await conn.query(
+			"UPDATE staff SET name = ?, job_function = ? WHERE code = ?",
+			[name, jobFunction, req.params.code],
+		);
 	} catch (error) {
 		throw error;
 	}
